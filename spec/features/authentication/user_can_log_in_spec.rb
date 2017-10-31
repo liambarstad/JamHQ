f0721621ef2882e1a3b365ad2013189bcb6fa4f5
Liam Barstad <liambarstad@gmail.com>
2017-10-31 14:13:33 -0600

    add rspec tests for regular/facebook login

---
+++ spec/features/authentication/user_can_log_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "user can log in" do
+  scenario "with built in credentials" do
+    user = create(:user)
+    visit "/"
+    click_on "Login"
+    fill_in "username", with: user.username
+    fill_in "password", with: "LOGIN"
+    click_on "Login"
+
+    expect(current_path).to eq("/")
+    expect(page).to_not have_content("Login")
+    expect(page).to have_content("Logout")
+  end
+
+  xscenario "with facebook" do
+    user = create(:user)
+    visit "/"
+    click_on "Login"
+    click_on "Login With Facebook"
+    fill_in "username", with: user.username
+    fill_in "password", with: "LOGIN"
+    click_on "Login"
+
+    expect(current_path).to eq("/")
+    expect(page).to_not have_content("Login")
+    expect(page).to have_content("Logout")
+  end
+end
