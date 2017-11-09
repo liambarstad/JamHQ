c8d4aa9c2efaf4cf933bfb4407ed6f6da062f92c
Liam Barstad <liambarstad@gmail.com>
2017-11-08 18:11:18 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "users#index"
+  root "welcome#index"
 
   get "/session", to: "session#show"
   put "/session", to: "session#update"
@@ -10,5 +10,16 @@ Rails.application.routes.draw do
   resources :session, only: [:new, :create]
 
   resources :users, only: [:index, :show]
+  resources :venues, only: [:index, :show]
+  resources :posts, only: [:index, :show]
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:create, :update, :destroy]
+      resources :comments, only: [:create, :update, :destroy]
+      resources :users, only: [:create, :update, :destroy]
+      resources :venues, only: [:create, :update, :destroy]
+    end
+  end
 
 end
