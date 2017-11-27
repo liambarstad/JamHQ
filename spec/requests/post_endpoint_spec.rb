594e96a59490a5e75915d71dbe6ef518921b9769
Liam Barstad <liambarstad@gmail.com>
2017-11-27 13:23:34 -0700

    move spec/api/v1 to spec/requests

--- spec/requests/post_endpoint_spec.rb
+++ spec/requests/post_endpoint_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "post endpoints" do
                          first_name: "Liam",
                          email: "liambarstad@gmail.com3")
       post = Post.new(body: "oh what a feeling")
-      token = Token_service.get_token
+      token = TokenService.get_token(email: user.email, password: "LOGIN")
 
       post "/api/v1/posts", headers: { token: token, body: post.body }
 
@@ -26,13 +26,12 @@ RSpec.describe "post endpoints" do
                          first_name: "Liam",
                          email: "liambarstad@gmail.com3")
       post = Post.new(body: "oh what a feeling")
-      token = Token_service.get_token
-
+      token = TokenSerivce.get_token(email: user.email, password: "LOGIN")
       post "/api/v1/posts", headers: { token: token, body: "" }
 
       saved_post = Post.find_by(body: post.body)
 
-      expect(response.status).to eq(204)
+      expect(response.status).to eq(500)
       expect(saved_post).to eq(nil)
     end
   end
@@ -44,7 +43,7 @@ RSpec.describe "post endpoints" do
                          first_name: "Liam",
                          email: "liambarstad@gmail.com4")
       post = user.posts.create(body: "oh what a feeling")
-      token = Token_service.get_token
+      token = TokenService.get_token(email: user.email, password: "LOGIN")
 
       put "/api/v1/posts/#{post.id}", headers: { token: token, body: "more of a feeling" }
       result = JSON.parse(response.body)
@@ -58,7 +57,7 @@ RSpec.describe "post endpoints" do
                          first_name: "Liam",
                          email: "liambarstad@gmail.com5")
       post = user.posts.create(body: "oh what a feeling")
-      token = Token_service.get_token
+      token = TokenService.get_token(email: user.email, password: "LOGIN")
 
       put "/api/v1/posts/#{post.id}", headers: { token: token, body: "" }
 
@@ -73,11 +72,11 @@ RSpec.describe "post endpoints" do
                          first_name: "Liam",
                          email: "liambarstad@gmail.com6")
       post = user.posts.create(body: "oh what a feeling")
-      token = Token_service.get_token
+      token = TokenService.get_token(email: user.email, password: "LOGIN")
 
       delete "/api/v1/posts/#{post.id}", headers: { token: token }
       new_post = Post.find_by(body: "oh what a feeling")
-
+      binding.pry
       expect(new_post).to eq(nil)
     end
   end
